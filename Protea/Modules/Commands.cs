f1a72d771bb7ee65cf50118a186e353a2c838ca6
EscoSD <javierjregido@gmail.com>
2024-09-20 13:54:57 +0200

    Refactor

--- Protea/Modules/Commands.cs
+++ Protea/Modules/Commands.cs
@@ -9,7 +9,6 @@ public class Commands (IJsonService jsonService): ModuleBase<SocketCommandContex
 	[Summary("Revela el tiempo que has pasado en canales de voz dentro de este servidor.")]
 	public async Task GetVcTimeAsync()
 	{
-		
 		var response = await jsonService.GetUserTimeAsync(Context.User.Username);
 		await ReplyAsync(response);
 	}
