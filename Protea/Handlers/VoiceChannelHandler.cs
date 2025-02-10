6746edca31fa9e3104be80925a4f9aae366da8dd
EscoSD <javierjregido@gmail.com>
2025-02-10 22:30:31 +0100

    fix.- Ignoring Bots Voice State Updates

--- Protea/Handlers/VoiceChannelHandler.cs
+++ Protea/Handlers/VoiceChannelHandler.cs
@@ -14,12 +14,12 @@ public class VoiceChannelHandler(DiscordSocketClient client, IVoiceChannelTimerS
 	
 	private async Task HandleUserVoiceStateUpdated(SocketUser user, SocketVoiceState before, SocketVoiceState after)
 	{
-		if (before.VoiceChannel == null)
+		if (!user.IsBot && before.VoiceChannel == null)
 		{
 			Console.WriteLine($"{user.Username} ha entrado a un canal de {after.VoiceChannel?.Guild.Name}");
 			vcTimerService.SaveVcEntry(user, after.VoiceChannel!.Guild.Id);
 
-		} else if (after.VoiceChannel == null)
+		} else if (!user.IsBot && after.VoiceChannel == null)
 		{
 			Console.WriteLine($"{user.Username} ha salido de un canal de {before.VoiceChannel?.Guild.Name}");
 			await vcTimerService.SaveUserTime(user, before.VoiceChannel!.Guild);
