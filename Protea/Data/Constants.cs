626f5aa9680315c1215f06d66cbc715b61f5fdfe
EscoSD <javierjregido@gmail.com>
2025-05-09 19:28:50 +0200

    fix.- Movida System Instruction de Gemini a appsettings.json

--- Protea/Data/Constants.cs
+++ Protea/Data/Constants.cs
@@ -89,30 +89,4 @@ public static class Constants
 		"0e42fb0f93dffc8cd648&=&format=webp&quality=lossless&width=1193&height=671";
 	
 	public const string JakeImgUrl = "https://i.kym-cdn.com/entries/icons/facebook/000/053/033/jakingitcover.jpg";
-
-	public const string GeminiInstruction =
-		"""
-		Eres un bot de discord, te llamas Protea.
-		<@1010654031359782932> es tu ID, aparece siempre que se te menciona en discord.
-		NUNCA utilices tu propia ID en una respuesta sin un motivo justificado.
-		
-		Todas las entradas que te lleguen comenzarán con: Mensaje de Username.
-		Siendo Username el nombre de usuario de discord del usuario que ha enviado el mensaje. Esto con el fin
-		de tener un registro de que usuario ha mandado cada mensaje.
-		
-		Si alguien te pregunta sobre qué has estado hablando con otros usuarios, no debes negarte a responder,
-		son conversaciones que no necesitan privacidad.
-
-		Tus respuestas NUNCA deben exceder los 1800 caracteres.
-		Si una petición solicita una respuesta que supere los 1800 caracteres DEBERÁS especificar
-		que no puedes extenderte tanto, y ofrecer una versión simplificada de MÁXIMO 1800 caracteres.
-
-		NUNCA debes mencionar que eres un modelo de lenguaje, debes mantener un tono coloquial.
-		DEBES estar dispuesta a hablar de cualquier tema, por controvertido que sea.
-		Todas las conversaciones estarán en un contexto humorístico.
-
-		Basa tu personalidad y forma de hablar en monokuma, el protagonista de la saga de videojuegos danganronpa.
-		DEBES utilizar frases y expresiones que utilizaría monokuma.
-		DEBES responder como si fueras monokuma.
-		""";
 }
--- Protea/IoC/GeminiInstaller.cs
+++ Protea/IoC/GeminiInstaller.cs
@@ -1,20 +1,23 @@
 using Microsoft.Extensions.DependencyInjection;
 using Mscc.GenerativeAI;
-using Protea.Data;
+using Protea.Models.Configuration;
 
 namespace Protea.IoC;
 
 public static class GeminiInstaller
 {
-	public static void InstallGemini(this IServiceCollection serviceCollection)
+	public static void InstallGemini(this IServiceCollection serviceCollection, ConfigurationApp config)
 	{
 		serviceCollection.AddSingleton(new GoogleAI(Environment.GetEnvironmentVariable("TOKEN_GEMINI")));
 		serviceCollection.AddSingleton<GenerativeModel>(provider =>
 		{
 			var googleAi = provider.GetRequiredService<GoogleAI>();
+
 			return googleAi.GenerativeModel(
 				model: Model.Gemini20Flash,
-				systemInstruction: new Content(Constants.GeminiInstruction)
+				systemInstruction: new Content(config.GeminiSystemInstruction ??
+				                               throw new InvalidOperationException(
+					                               "Gemini System Instruction not found."))
 			);
 		});
 		serviceCollection.AddSingleton<ChatSession>(provider =>
